d17e8ce4eabd5bca4b604becd8f83fe723afe490
noreen-h <92766174+noreen-h@users.noreply.github.com>
2024-09-05 14:28:22 -0400

    Add files via upload

---
+++ cellStatistics_20240813_speed_length.m
@@ -0,0 +1,87 @@
+clear all;
+
+% Define image dimensions and load oxygen image
+SX=25091; SY=18749;
+
+% read oxygen level
+oxy= (abs(imread('oxy_stack_11_may_small_20_gauss0006.tif')));
+oxy_gray=im2gray(oxy);
+
+% smoothen the oxygen level
+oxy_gauss=imgaussfilt(oxy_gray,20);
+
+% Make the mask
+mask0=oxy_gauss>0.035;
+xx=1:size(mask0,2);
+yy=1:size(mask0,1);
+[XX,YY]=meshgrid(xx,yy);
+mask0(XX>950)=0;
+mask_large = imresize(mask0,[SY nan]);
+oxy_large = imresize(oxy_gauss,[SY nan]);
+[gmx,gmy]=gradient(mask_large);
+gm=sqrt(gmx.^2+gmy.^2);
+cx=[]; cy=[];
+for i=1:size(gm,1)
+    for j=1:size(gm,2)
+        if gm(i,j)>0.001
+            cx=[cx,i];
+            cy=[cy,j];
+        end
+    end
+end
+
+% Load all cell tracks
+load('cellStats_wSpeed.mat');
+
+% Find the tracks within the gradient mask
+idM=[]; % id of the tracks within the mask
+for i=1:length(xT)
+    yP=min(size(mask_large,1),max(1,round(yT(i))));
+    xP=min(size(mask_large,2),max(1,round(xT(i))));
+    if mask_large(yP,xP)
+        idM=[idM,i];
+    end
+end
+
+% Extract ss (cell size) and vvInt (integrated track length) for cells within gradient mask
+ss=ss(idM);
+vvInt=vvInt(idM);
+xT=xT(idM);
+yT=yT(idM);
+
+i_ll=1./sqrt(ss);
+i_vvInt=1./vvInt;
+
+% Define bin edges based on ss
+binEdges = linspace(min(i_ll), max(i_ll),30);  % Adjust the number of bins as needed
+mean_i_vvInt = zeros(1, length(binEdges)-1);
+std_i_vvInt = zeros(1, length(binEdges)-1);
+
+clear counts;
+for i = 1:length(binEdges)-1
+    indices = i_ll >= binEdges(i) & i_ll < binEdges(i+1); % indices of tracks within the mask
+    counts(i) = sum(indices); % N added
+    if any(indices)
+        mean_i_vvInt(i) = mean(i_vvInt(indices));
+        std_i_vvInt(i) = std(i_vvInt(indices))/sqrt(sum(indices));  % Standard error   
+    end
+end
+
+% Plotting mean cell size vs integrated trajectory length with binning
+figure;
+scatter(binEdges(1:end-8), mean_i_vvInt(1:end-7), 100, counts(1:end-7), 'filled'); % N added
+
+xlabel('1/Mean Cell Length (1/ll)');
+ylabel('1/Integrated Trajectory Length (1/vvInt)');
+title('Mean Cell Length vs Integrated Trajectory Length, Binned');
+set(gca, 'fontsize', 21); 
+
+colormap('jet'); % N added
+
+%  Error Bars N added
+hold on;
+errorbar(binEdges(1:end-8), mean_i_vvInt(1:end-7), std_i_vvInt(1:end-7), 'vertical', 'k', 'LineStyle', 'none');
+
+%Appearance - N added
+colorbar;
+ylabel(colorbar, 'Count');
